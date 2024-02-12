c6bf84a3a3e5c4a6a4aed2c3510d5d7dd87bf7e5
Alexander_Dulalaev <camel1985@mail.ru>
2024-02-12 08:52:56 +0300

    minor updates and add lesson files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,12 @@
 import pytest
 from selene import browser
 
-BASE_URL = 'https://github.com'
-
 
 @pytest.fixture(autouse=True)
 def browser_open():
     browser.config.window_width = 1080
     browser.config.window_height = 720
-    browser.config.base_url = BASE_URL
+    browser.config.base_url = 'https://github.com'
 
     yield
 
